96159c3ec9103a86a0fb7fdaa3572a5f9913c901
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-06-04 15:39:21 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+ * current style
+ *
+ *    tt, code, pre {
+ *      font-family: Consolas, "Liberation Mono", Monaco, Courier, monospace;
+ *    }
+ */
+
+window.addEventListener("load", function() {
+  // handle pjax
+  var time;
+  var pjaxele = document.getElementById('js-repo-pjax-container');
+
+  // TODO find better way to handle DOMSubtreeModified
+  // https://developer.mozilla.org/en-US/docs/Web/Reference/Events
+  pjaxele.addEventListener('DOMSubtreeModified', function() {
+    if (time) {
+      clearTimeout(time);
+    }
+
+    // this will always execute twice, due to 2 events. can offset but unsure what the
+    // exact offset would be, seems like 2 though
+    time = setTimeout(function() {
+      revertToCourier(document);
+      time = null;
+    }, 10);
+  });
+
+  // regular page loads
+  revertToCourier(document);
+}, true);
+
+function revertToCourier(document) {
+  setFontFamily(document.querySelector('.blob-line-code pre'));
+  setFontFamily(document.querySelector('.blob-line-nums'));
+}
+
+/*
+ * set font-family for element
+ */
+
+function setFontFamily(ele) {
+  if (!ele) {
+    return;
+  }
+
+  ele.style['font-family'] = 'Consolas, "Liberation Mono", Courier, Monaco, monospace';
+}
+
